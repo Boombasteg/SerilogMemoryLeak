4c154727acb2e89ae8436bd0fc1b203b30e60756
dhlukhatarenka <dhlukhatarenka@scnsoft.com>
2018-02-02 16:48:20 +0300

    add application

---
+++ WebCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ServiceFabric.Services.Runtime;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WebCore
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                ServiceRuntime.RegisterServiceAsync("WebCoreType", context => new WebCore(context)).GetAwaiter().GetResult();
+                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(WebCore).Name);
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
+        }
+    }
+}
---
+++ WebCore/WebCore.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Serilog;
+
+namespace WebCore
+{
+    internal sealed class WebCore : StatelessService
+    {
+        public WebCore(StatelessServiceContext context) : base(context) { }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new WebListenerCommunicationListener(serviceContext, "APIServiceEndpointHttp",
+                        (url, listener) =>
+                        {
+                            return ConfigureWebHost(serviceContext, url, listener)
+                                .ConfigureAppConfiguration((hostingContext, config) =>
+                                {
+                                    //var configuration = config
+                                    //    .AddJsonFile("appsettings.json", true, true)
+                                    //    .AddEnvironmentVariables()
+                                    //    .Build();
+
+                                    //Log.Logger = new LoggerConfiguration()
+                                    //    .ReadFrom
+                                    //    .Configuration(configuration)
+                                    //    .CreateLogger();
+                                })
+                                .ConfigureLogging((hostingContext, logging) =>
+                                {
+                                    //logging.AddSerilog(dispose: true);
+                                    logging.AddConsole();
+                                    logging.AddDebug();
+                                })
+                                .Build();
+                        }))
+            };
+        }
+
+        private static IWebHostBuilder ConfigureWebHost(StatelessServiceContext serviceContext, string url, AspNetCoreCommunicationListener listener)
+        {
+            return new WebHostBuilder()
+                .UseHttpSys()
+                .ConfigureServices(services => services.AddSingleton(serviceContext))
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                .UseUrls(url);
+        }
+    }
+}
